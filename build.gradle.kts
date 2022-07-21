d50e9cef00bf0b79ae20a6ad23143b41d3addd59
Jimmy Morales <me@jimmymorales.dev>
2022-07-21 13:40:41 -0600

    chore: Configture Detekt and JVM version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,28 @@
+import io.gitlab.arturbosch.detekt.Detekt
+
 plugins {
-    kotlin("jvm") version "1.7.10"
+	kotlin("jvm") version "1.7.10"
+	id("io.gitlab.arturbosch.detekt") version "1.21.0"
 }
 
 group = "dev.jimmymorales"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenCentral()
+	mavenCentral()
+}
+
+kotlin {
+	jvmToolchain {
+		languageVersion.set(JavaLanguageVersion.of(18))
+		vendor.set(JvmVendorSpec.AZUL)
+	}
+}
+
+val detektJvmVersion = JavaVersion.VERSION_1_8.toString()
+tasks.withType<Detekt>().configureEach {
+	jvmTarget = detektJvmVersion
+}
+tasks.withType<io.gitlab.arturbosch.detekt.DetektCreateBaselineTask>().configureEach {
+	jvmTarget = detektJvmVersion
 }
